350349dff9f6c00d96448f8b4d3216412fc8b4a8
Pablo Alcocer <palco003@fiu.edu>
2017-10-05 18:35:40 -0400

    Fix whitespace

--- api/routes/invites.js
+++ api/routes/invites.js
@@ -34,16 +34,16 @@ module.exports = function(server, db) {
 
 	var response ='<!DOCTYPE html>' +
 		'<html lang="en">' +
-		'<head>' +
-		'<meta http-equiv="X-UA-Compatible" content="IE=edge">' +
-		'<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">' +
-		'<meta charset="utf-8">' +
-		'<title>[[subject]]</title>' +
-		'<!--[if lt IE 9]>' +
-		'<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>' +
-		'<![endif]-->' +
-		'</head>' +
-		'<body>[[body]]</body>' +
+            '<head>' +
+                '<meta http-equiv="X-UA-Compatible" content="IE=edge">' +
+                '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">' +
+                '<meta charset="utf-8">' +
+                '<title>[[subject]]</title>' +
+                '<!--[if lt IE 9]>' +
+                '<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>' +
+                '<![endif]-->' +
+            '</head>' +
+            '<body>[[body]]</body>' +
 		'</html>';
 
 	server.get(apiPrefix + '/invite/:id/accept', function (req, res, next) {
